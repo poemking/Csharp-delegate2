027f117efdff2f188acd1aeaa04ab5b4a2cfc514
Cheng-Han, Yang <poemofking@gmail.com>
2016-08-01 17:26:17 +0800

    add delegate of demo to easy use the "delegate"

---
+++ WinProj_delegate2/WinProj_delegate2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WinProj_delegate2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoser = new System.Windows.Forms.Button();
+            this.btnFriend = new System.Windows.Forms.Button();
+            this.btnGirl = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLoser
+            // 
+            this.btnLoser.Location = new System.Drawing.Point(13, 41);
+            this.btnLoser.Name = "btnLoser";
+            this.btnLoser.Size = new System.Drawing.Size(75, 23);
+            this.btnLoser.TabIndex = 0;
+            this.btnLoser.Text = "魯蛇借10元";
+            this.btnLoser.UseVisualStyleBackColor = true;
+            this.btnLoser.Click += new System.EventHandler(this.btnLoser_Click);
+            // 
+            // btnFriend
+            // 
+            this.btnFriend.Location = new System.Drawing.Point(131, 41);
+            this.btnFriend.Name = "btnFriend";
+            this.btnFriend.Size = new System.Drawing.Size(75, 23);
+            this.btnFriend.TabIndex = 1;
+            this.btnFriend.Text = "死黨借100";
+            this.btnFriend.UseVisualStyleBackColor = true;
+            this.btnFriend.Click += new System.EventHandler(this.btnFriend_Click);
+            // 
+            // btnGirl
+            // 
+            this.btnGirl.Location = new System.Drawing.Point(249, 41);
+            this.btnGirl.Name = "btnGirl";
+            this.btnGirl.Size = new System.Drawing.Size(75, 23);
+            this.btnGirl.TabIndex = 2;
+            this.btnGirl.Text = "正妹借30萬";
+            this.btnGirl.UseVisualStyleBackColor = true;
+            this.btnGirl.Click += new System.EventHandler(this.btnGirl_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(13, 70);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(311, 227);
+            this.txtResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 309);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnGirl);
+            this.Controls.Add(this.btnFriend);
+            this.Controls.Add(this.btnLoser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoser;
+        private System.Windows.Forms.Button btnFriend;
+        private System.Windows.Forms.Button btnGirl;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ WinProj_delegate2/WinProj_delegate2/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinProj_delegate2
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 定義一個自訂借錢動作的委派(想像成是定義一個介面，規定參數及回傳型態就對了)
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        delegate string CustomAction(string amount);
+
+        CustomAction customAction;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGirl_Click(object sender, EventArgs e)
+        {
+            customAction = LendToGirl;
+            LendAction("30萬", customAction);
+        }
+
+        private void btnFriend_Click(object sender, EventArgs e)
+        {
+            customAction = LendToFriend;
+            LendAction("100", customAction);
+        }
+
+        private void btnLoser_Click(object sender, EventArgs e)
+        {
+            customAction = LendToLoser;
+            LendAction("10", customAction);
+        }
+
+        /// <summary>
+        /// 借錢動作
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="customAct"></param>
+        private void LendAction(string amount, CustomAction customAct)
+        {
+            txtResult.Text = string.Empty;
+
+            //決定要借出的金額
+            string finalAmount;
+
+            //我們不需要知道這個customAct到底是什麼
+            //反正他跑完會回傳一個我們要的東西就對了
+            //在這裡回傳的就是最終借出金額
+            finalAmount = customAct(amount); //這行會跳進去+入的委派function
+
+            string commonRes;
+            if (!string.IsNullOrEmpty(finalAmount))
+            {
+                commonRes = string.Format("借出{0}元", finalAmount);
+            }
+            else
+            {
+                commonRes = "掉頭就走";
+            }
+
+            txtResult.Text += commonRes;
+        }
+
+        /// <summary>
+        /// 借錢給正妹的自訂動作
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        private string LendToGirl(string amount)
+        {
+            //自訂動作：跟正妹狂聊，最後決定借五百萬
+            var res =
+    @"詢問正妹：真的只要借{0}嗎?夠不夠啊?
+詢問正妹：要幫妳買點數卡嗎?
+詢問正妹：可以加妳的Line嗎?
+詢問正妹：妳幾歲呀?
+詢問正妹：妳住哪?
+詢問正妹：妳有男朋友嗎?
+詢問正妹：妳三圍多少?
+詢問正妹：禮拜六有空嗎?
+...
+.....
+....
+哇!服務這麼好喔!
+....
+.....
+GGInInDer
+OK~{1}沒問題!
+....
+...去提款機領{1}元
+";
+            var finalAmount = "五百萬";
+            txtResult.Text = string.Format(res, amount, finalAmount);
+
+            //回傳最後決定的金額
+            return finalAmount;
+        }
+
+        private string LendToFriend(string amount) 
+        {
+            var res = @"幹...
+            (錢包掉出{0}元)";
+
+            txtResult.Text = string.Format(res, amount);
+            return amount;
+        }
+
+        private string LendToLoser(string amount) 
+        {
+            return string.Empty;
+        }
+    }
+}
